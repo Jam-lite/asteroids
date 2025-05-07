2b0d1f267759a6cdc29abd53764cf77da4b9ddaa
Jamlite-dev <jjohnston5631@gmail.com>
2025-05-07 18:50:45 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
     # === GAME LOOP ===
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # Fill the screen with black (RGB: 0, 0, 0)
-        screen.fill((0, 0, 0))
+            
+        # Fill the screen with black
+        screen.fill("black")
 
         # Update (flip) the display
         pygame.display.flip()
